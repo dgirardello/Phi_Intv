f49ac239b8d2e154483a1f4a1cfaac7ca31890af
dgirardello <nanodag@gmail.com>
2026-09-08 15:42:06 +0100

    Initial commit

---
+++ config/env.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://qa-env-calculator.internal.corp"
+DEFAULT_BROWSER = "chrome"
+IMPLICIT_WAIT = "5s"
+SELENIUM_TIMEOUT = "10s"
+
+VALID_USER = "qa_analyst@corp.internal"
+VALID_PASSWORD = "SecurePass2026!"
